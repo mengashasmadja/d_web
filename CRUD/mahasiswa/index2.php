5b4a4d942a73f2ceb68253d6f97f617994c75941
mengashasmadja <mengashasmadja010@gmail.com>
2020-12-29 20:08:20 +0700

    Web Mengas

---
+++ CRUD/mahasiswa/index2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Kampus</title>
+	<!-- <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+
+	<style>
+		
+	</style>
+</head>
+<body>
+
+<div class="container">
+		<div class="alert alert-info">Data Mahasiswa</div>
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>No</th>
+					<th>NIM</th>
+					<th>Nama Mahasiswa</th>
+					<th>Prodi</th>
+					<th>Aksi</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<?php
+					require '../koneksi.php';
+					$query = "SELECT * FROM mahasiswa";
+					// Jalankan query di atas
+					$result = mysqli_query($link, $query);
+					$no =1;
+					while ($isi = mysqli_fetch_object($result)) {
+				?>
+
+				<tr>
+					<td><?= $no++; ?></td>
+					<td><?= $isi->nim; ?></td>
+					<td><?= $isi->nama_mahasiswa; ?></td>
+					<td><?= $isi->prodi; ?></td>
+					<td>
+						<a href="" class="btn btn-warning">Edit</a>
+						<a href="" class="btn btn-danger">Delete</a>
+					</td>
+				</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+
+    </body>
+</html>
